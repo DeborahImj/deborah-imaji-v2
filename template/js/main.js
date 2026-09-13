ef6f55539702b28c383208d84a522fb68ef3b967
DeborahImj <deborahimajieng@gmail.com>
2026-09-13 20:03:22 +0100

    Built custom cursor

--- template/js/main.js
+++ template/js/main.js
@@ -6,3 +6,18 @@ window.addEventListener('load', () => {
   
   preloader.classList.remove('is-inview');
 });
+
+// CURSOR
+
+const cursor = document.querySelector('.cursor');
+
+const halfWidth = cursor.offsetWidth / 2;
+const halfHeight = cursor.offsetHeight / 2;
+
+window.addEventListener('pointermove', (e) => {
+  const x = e.clientX - halfWidth;
+  const y = e.clientY - halfHeight;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
